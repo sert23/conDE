4424035032f743f369b06612cdc3ae00f58ea075
eaparicio <erapariciopuerta@gmail.com>
2019-07-25 17:39:40 +0200

    volcano plot functions

--- plot/urls.py
+++ plot/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # url(r'^$', views.new_upload, name='multi_new'),
     url(r'^heatmap/recalc$', views.heatmap_recalculate, name='hm_recalc'),
     url(r'^heatmap/[A-za-z0-9]+', views.Heatmap.as_view(), name="heatmap"),
+    url(r'^volcano/recalc$', views.volcano_recalculate, name='volc_recalc'),
+    url(r'^volcano/[A-za-z0-9]+', views.Volcano.as_view(), name="volcano"),
 
 
             ]
--- plot/views.py
+++ plot/views.py
@@ -25,6 +25,21 @@ def heatmap_recalculate(request):
     os.system(" ".join(call_list))
     return JsonResponse({"test":"ejejhe"})
 
+def volcano_recalculate(request):
+    id = request.GET.get('id', None)
+    folder = os.path.join(MEDIA_ROOT, id)
+    path_to_config = os.path.join(folder, "plot_config.json")
+    with open(path_to_config) as f:
+        config = json.load(f)
+
+    title = request.GET.get('title', None)
+    if title:
+        config.update({"title": title})
+    with open(path_to_config, 'w') as f:
+        json.dump(config, f)
+    call_list = [RSCRIPT_PATH, os.path.join(RPLOTS_PATH, "volcano.R"), path_to_config]
+    os.system(" ".join(call_list))
+    return JsonResponse({"test":"ejejhe"})
 
 
 class Heatmap(FormView):
@@ -51,4 +66,27 @@ class Heatmap(FormView):
                        "heatmap_url": heatmap_url
                        })
 
+class Volcano(FormView):
+    #template_name = 'bench.html'
+    #form_class = sRNABenchForm
+    #success_url = reverse('photos:multi_start')
+
+    def get_form_kwargs(self):
+        '''This goes in the Update view'''
+        kwargs = super(Volcano, self).get_form_kwargs()  # put your view name in the super
+
+        #kwargs["folder"] = self.request.path
+        return kwargs
+
+    def get(self, request,**kwargs):
+        path = request.path
+        folder = path.split("/")[-1]
+        volcano_url = os.path.join(MEDIA_URL,folder,"volcano.html")
+
+
+
+        return render(self.request, 'heatmap_template.html',
+                      {"job_id": folder,
+                       "volcano_url": volcano_url
+                       })
 
