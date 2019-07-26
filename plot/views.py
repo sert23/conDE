7675b748391a13df729a095a8dc36fdddce4d2df
eaparicio <erapariciopuerta@gmail.com>
2019-07-26 19:38:57 +0200

    adding random redirection to avoid iframe caching

--- plot/views.py
+++ plot/views.py
@@ -17,7 +17,7 @@ def random_redirect(request):
 
 def make_random_url(id,plot):
     random_url = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    url = os.path.join(SUB_SITE,"plot",random_url)
+    url = os.path.join(SUB_SITE,"plot","random",random_url)
     return url+"?plot=" + plot + "&id=" + id
 
 
