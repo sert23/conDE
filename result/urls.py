26422ded7bdb2e5b185f768a7a2de715c59c22b3
eaparicio <erapariciopuerta@gmail.com>
2019-08-07 00:23:51 +0200

    adding tag for reverse

--- result/urls.py
+++ result/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^ajax_graph$', views.get_plot_content, name='ajax_graph'),
     url(r'^ajax_recalc$', views.ajax_recalculate, name='ajax_recalc'),
     url(r'^[A-za-z0-9]+', views.DEresult.as_view(), name="DEresult"),
+    url(r'^', views.DEresult.as_view(), name="result"),
 
 
 
